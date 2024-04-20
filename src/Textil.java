7c7cfce6fcf0b745c1a97e7156584165527fcd3f
Mark <m.alvarez@sapalomera.cat>
2024-04-20 21:16:28 +0200

    Classe textil creada

---
+++ src/Textil.java
@@ -0,0 +1,16 @@
+class textil extends Producte {
+
+    String composicio;
+
+    public textil(String nom, String codiBarres, int preu, String composicio) {
+        super(nom, codiBarres, preu);
+        this.composicio = composicio;
+    }
+
+    @Override
+    public String getNom() {return this.nom;}
+    @Override
+    public String getCodiBarres() {return this.codiBarres;}
+    @Override
+    public int getPreu() {return this.preu;}
+}
