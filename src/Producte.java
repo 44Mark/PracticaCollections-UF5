5d390c9115a9fd3140111bed94ef73d0baa19122
Mark <m.alvarez@sapalomera.cat>
2024-04-20 21:19:01 +0200

    Classe producte amb segon menu i metodes d'introducció

--- src/Producte.java
+++ src/Producte.java
@@ -52,7 +52,7 @@ class TestProducte {
         sc.nextLine();
         switch (inici) {
             case 1:
-                //introduirProducte();
+                introduirProducte();
                 break;
             case 2:
                 //passarCaixa();
@@ -67,5 +67,105 @@ class TestProducte {
                 System.out.println("Opció no vàlida, prova un altre cop");
                 menuTiquet();
         }
+    }
+
+    //L'user escull que tipus de producte vol introduir
+    public static void introduirProducte() {
+        System.out.println("---------------");
+        System.out.println("--PRODUCTE---");
+        System.out.println("---------------");
+        System.out.println("1) Alimentació");
+        System.out.println("2) Tèxtil");
+        System.out.println("3) Electrònica");
+        System.out.println("0) Tornar");
+
+        int tipusProducte = sc.nextInt();
+        sc.nextLine();
+        switch (tipusProducte) {
+            case 1:
+                introduirAlimentacio();
+                break;
+            case 2:
+                introduirTextil();
+                break;
+            case 3:
+                introduirElectronica();
+                break;
+            case 0:
+                System.out.println("Tornant...");
+                System.out.println();
+                menuTiquet();
+                break;
+            default:
+                System.out.println("Opció no vàlida, prova un altre cop");
+                introduirProducte();
+                break;
+        }
+    }
+
+    //Metode per introduir productes de tipus textil
+    protected static void introduirTextil() {
+        System.out.println("Afegint textil: ");
+        System.out.println("Omple les següents dades: ");
+
+        System.out.println("Nom producte: ");
+        String nom = sc.nextLine();
+        System.out.println("Codi de barres: ");
+        String codiBarres = sc.nextLine();
+        System.out.println("Preu: ");
+        int preu = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Composicio del producte: ");
+        String composicio = sc.nextLine();
+
+        textil t = new textil(nom, codiBarres, preu, composicio);
+        carreto.add(t);
+        menuTiquet();
+    }
+
+    //Metode per introduir productes de tipus alimentació
+    protected static void introduirAlimentacio() {
+        System.out.println("Afegint alimentació: ");
+        System.out.println("Omple les següents dades: ");
+
+        System.out.println("Nom producte: ");
+        String nom = sc.nextLine();
+        System.out.println("Codi de barres: ");
+        String codiBarres = sc.nextLine();
+        System.out.println("Preu: ");
+        int preu = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Data de caducitat(dd/MM/yyyy): ");
+        String dataCaducitat = sc.nextLine();
+
+        alimentacio a = new alimentacio(nom, codiBarres, preu, dataCaducitat);
+        carreto.add(a);
+        menuTiquet();
+    }
+
+    //Metode per introduir productes de tipus electrònica
+    protected static void introduirElectronica() {
+        System.out.println("Afegint electrònica: ");
+        System.out.println("Omple les següents dades: ");
+
+        System.out.println("Nom producte: ");
+        String nom = sc.nextLine();
+        System.out.println("Codi de barres: ");
+        String codiBarres = sc.nextLine();
+        System.out.println("Preu: ");
+        int preu = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Garantia(dies): ");
+        int garantia = sc.nextInt();
+
+        electronica e = new electronica(nom, codiBarres, preu, garantia);
+        carreto.add(e);
+        menuTiquet();
     }
 }
